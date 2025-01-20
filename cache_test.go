08710e6e54db21f78d57ff08571e6cd953dffbe2
dm <dmitrij.demidovich@gmail.com>
2025-01-20 11:45:13 +0300

    feat: add basic tests

--- cache_test.go
+++ cache_test.go
@@ -8,16 +8,58 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func cacheInstance(size int, gcIntervalSeconds int) *Cache {
+	ctx := context.TODO()
+	return NewCacheWithConfig(
+		ctx,
+		Config{
+			Size:       size,
+			GcInterval: time.Duration(gcIntervalSeconds) * time.Second,
+		},
+	)
+}
+
 func TestConfig(t *testing.T) {
-	ctx := context.Background()
+	ctx := context.TODO()
 	c := NewCacheWithConfig(
 		ctx,
 		Config{
-			Size:       10,
-			GcInterval: 10 * time.Second,
+			Size:       5,
+			GcInterval: 5 * time.Second,
 		},
 	)
 
-	assert.Equal(t, c.size, 10)
-	assert.Equal(t, c.gcInterval, 10*time.Second)
+	assert.Equal(t, c.size, 5)
+	assert.Equal(t, c.gcInterval, 5*time.Second)
+}
+
+func TestGetSet(t *testing.T) {
+	c := cacheInstance(5, 5)
+	c.Set("key1", "val1")
+	val, ok := c.Get("key1")
+
+	assert.Equal(t, "val1", val)
+	assert.Equal(t, true, ok)
+}
+
+func TestGetMissing(t *testing.T) {
+	c := cacheInstance(5, 5)
+	val, ok := c.Get("missingKey")
+
+	assert.Equal(t, "", val)
+	assert.Equal(t, false, ok)
+}
+
+func TestOversize(t *testing.T) {
+	c := cacheInstance(2, 1)
+	c.Set("key1", "val1") // Это вычистит GC
+	c.Set("key2", "val2")
+	c.Set("key3", "val3")
+
+	time.Sleep(2 * time.Second)
+
+	val1, ok1 := c.Get("key1")
+
+	assert.Equal(t, "", val1)
+	assert.Equal(t, false, ok1)
 }
